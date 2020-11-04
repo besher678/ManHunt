28dbf8c59d4207360b6d37ce22d85ebc022cd68d
WolfDen133 <61922721+WolfDen133@users.noreply.github.com>
2020-11-04 01:50:13 +0000

    Update Main.php

--- ManHunt/src/ManHunt/ManHunt/Main.php
+++ ManHunt/src/ManHunt/ManHunt/Main.php
@@ -9,81 +9,233 @@ use pocketmine\plugin\PluginBase;
 
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerDeathEvent;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
+use pocketmine\math\Vector3;
+
 use pocketmine\event\Listener;
 
-class Main extends PluginBase
-{
-    public $speedrunner = [];
-    public function onCommand(CommandSender $sender, Command $cmd, String $label, array $args): bool
-    {
+use pocketmine\utils\Config;
 
-        switch ($cmd->getName()) {
-            case "hvs":
-                if ($sender instanceof Player) {
-                    if ($sender->hasPermission("HvS.use")) {
-                        $this->openHvS($sender);
-                    } else {
-                        $sender->sendMessage("§cYou do not have permission to execute this command!");
-                    }
-                } else {
-                    $sender->sendMessage("§cPlease use this command in-game!");
-                    break;
-                }
-        }
-        return true;
-    }
-    public function openHvS($player)
+class Main extends PluginBase implements Listener{
+
+    public function PName()
     {
-        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-        $form = $api->createSimpleForm(function (Player $player, int $data = null) {
-            $result = $data;
-            if ($result === null) {
-                return true;
-            }
-            switch ($result) {
-                case 0:
-                    $this->openHunter($player);
-                    break;
-                case 1:
-                    break;
-            }
-        });
-        $form->setTitle("§l§bHvSUI");
-        $dname = $player->getDisplayName();
-        $form->setContent("§ePlease chose a functon!");
-        $form->addButton("§l§aSet Hunter!", 0, "textures/items/diamond_sword");
-        $form->addButton("§cClose", 0, "textures/ui/realms_red_x");
-        $form->sendToPlayer($player);
-        return $form;
-    }
-    public function openHunter($player)
-    {
-        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-        $form = $api->createCustomForm(function (Player $player, array $data = null) {
-            $result = $data;
-            if ($result === null) {
-                return true;
-            }
-            $list = [];
-            foreach ($this->getServer()->getOnlinePlayers() as $players) {
-                $list[] = $players->getName();
-            }
-            $target = $this->playerList[$player->getName()][$data[0]];
-            $this->speedrunner[$target] = $target;
-            $player->sendMessage("§eSet Hunter to §b" . $target);
-        });
-        $list = [];
+        $list = array();
         foreach ($this->getServer()->getOnlinePlayers() as $players) {
             $list[] = $players->getName();
         }
-        $this->playerList[$player->getName()] = $list;
-        $form->setTitle("§l§bHunterUI");
-        $form->addDropdown("Pick a hunter!", $list);
-        $form->sendToPlayer($player);
-        return $form;
+        return $list;
     }
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+		@mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
+        $this->getResource("config.yml");
+        $config->set("GS", "false");
+        $config->save();
+    }
+    public function onDeath(PlayerDeathEvent $event){
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        if ($config->get("GS") == "true"){
+            if (!$e = $config->get("SR")){
+                $player = $event->getPlayer();
+                if (!$config->get("SR") == $player->getName()){
+                    foreach ($this->getServer()->getOnlinePlayers() as $p){
+                        $p->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+                        $p->sendMessage("§aCongratulations Hunters, you won the game!");
+                        $config->set("GS", "false");
+                        $config->save();
+                    }  
+                } 
+            } else {
+                $player = $event->getPlayer();
+                if ($config->get("SR") == $player->getName()){
+                    foreach ($this->getServer()->getOnlinePlayers() as $p){
+                        $p->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+                        $p->sendMessage("§cUnfortunately you where killed, there for you lost the game sorry!");
+                        $config->set("GS", "false");
+                        $config->save();
+                    }  
+                } 
+            }
+        }
+    }
+    
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+
+		switch($cmd->getName()){
+			case "hvs":
+			if($sender instanceof Player){
+                if($sender->hasPermission("HvS.use")){
+					$this->openHvS($sender);
+				} else {
+					$sender->sendMessage("§cYou do not have permission to execute this command!");
+				}
+			} else {
+				$sender->sendMessage("§cPlease use this command in-game!");
+			}
+		}
+	return true;
+    }
+    public function openHvS($player){
+		$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+		$form = $api->createSimpleForm(function (Player $player, int $data = null){
+			$result = $data;
+			if($result === null){
+				return true;
+            }
+            $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+			switch($result){
+                case 0:
+                    if($config->get("GS") == "false"){
+                        $this->openHunter($player);
+                        $config->set("World", "");
+                    } else {
+                        $player->sendMessage("§cThe game has allready started!");
+                    }
+                break;
+                case 1:
+                    if($config->get("GS") == "false"){
+                        $this->openWorld($player);
+                    } else {
+                        $player->sendMessage("§cThe game has allready started!");
+                    }
+                break;
+                case 2:
+                    if($config->get("GS") == "false"){
+                        if(!$this->getConfig()->get('SR') == ""){
+                            $config->set("GS", "true");
+                            $config->save();
+                            $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+                            $speedrunner = $config->get('SR');
+                            $world = $config->get("World");
+                            $world = $world;
+                            foreach($this->getServer()->getOnlinePlayers() as $p){
+                                if ($config->get("SR") == $player->getName()){
+                                    if ($config->get("World") == ""){
+                                        $tp = $p->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+                                    } else {
+                                        $tp = $p->teleport($this->getServer()->getLevelByName($world)->getSafeSpawn());
+                                    }
+                                    sleep (1);
+                                    $player->sendTitle("§c3");
+                                    sleep (1);
+                                    $player->sendTitle("§e2");
+                                    sleep (1);
+                                    $player->sendTitle("§a1");
+                                    sleep (1);
+                                    $player->sendTitle("§bYou are a \n§aSpeedRunner, §bjust survive.");
+                                    $player->sendTip("§3Mission: §aSurvive and beat the game!");
+                                    $name = $player->getName();
+                                    $player->sendMessage("§aGood luck " . $name);
+                                } else {
+                                    if ($config->get("World") == ""){
+                                        $tp = $p->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+                                    } else {
+                                        $tp = $p->teleport($this->getServer()->getLevelByName($world)->getSafeSpawn());
+                                    }
+                                    sleep (1);
+                                    $player->sendTitle("§c3");
+                                    sleep (1);
+                                    $player->sendTitle("§e2");
+                                    sleep (1);
+                                    $player->sendTitle("§a1");
+                                    sleep (1);
+                                    $player->sendTitle("§bYou are a \n§cHunter");
+                                    $player->sendTip("§3Mission: §cKill §c". $config->get("SR"));
+                                    $name = $player->getName();
+                                    $player->sendMessage("§aGood luck " . $name);
+                                } 
+                            }
+                        }else{
+                            $player->sendMessage("§cThere is no SpeedRunner set!");
+                        }
+                    } else {
+                        $player->sendMessage("§cThe game has allready started!");
+                    }
+                break;
+                case 3:
+                    if ($config->get("GS") == "true"){
+                        foreach ($this->getServer()->getOnlinePlayers() as $p){
+                            $p->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+                        }
+                        $config->set("GS", "false");
+                        $config->save();
+                    } else {
+                        $player->sendMessage("§cGame hasent started yet!");
+                    }
+                break;
+                case 4:
+                break;
+			}
+		});
+		$form->setTitle("§l§bHvSUI");
+		$dname = $player->getDisplayName();
+		$form->setContent("§ePlease chose a functon!");
+        $form->addButton("§l§9Set Hunter!\n§r§eChoose the hunter!", 0, "textures/items/diamond_sword");
+        $form->addButton("§l§9Set World!\n§r§eChoose the world!", 0, "textures/ui/world_glyph_color");
+        $form->addButton("§l§aStart!\n§r§eStarts the game!", 0, "textures/ui/realms_green_check");
+        $form->addButton("§l§aStop!\n§r§eStop the game!", 0, "textures/ui/crossout");
+		$form->addButton("§cClose", 0, "textures/ui/realms_red_x");
+		$form->sendToPlayer($player);
+		return $form;		
+    }
+    public function openHunter($player){
+		$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+		$form = $api->createCustomForm(function (Player $player, array $data = null){
+			$result = $data;
+			if($result === null){
+				return true;
+            }
+            $ps = $this->PName();
+            $p = $ps[$data[0]];
+            $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+            $config->set("SR", $p);
+            $config->save();
+            $player-> sendMessage("§eSet Hunter to: §b" . $p);
+            $this->openHvS($player);
+        });
+        $list = [];
+        foreach ($this->getServer()->getOnlinePlayers() as $players){
+            $list[] = $players->getName();
+        }
+        $this->playerList[$player->getName()] = $list;
+		$form->setTitle("§l§bHunterUI");
+        $form->addDropdown("§ePick a hunter!", $list);
+		$form->sendToPlayer($player);
+		return $form;
+    }
+    public function openWorld($player){
+		$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+		$form = $api->createCustomForm(function (Player $player, array $data = null){
+			$result = $data;
+			if($result === null){
+				return true;
+            }
+            $list = array();
+            foreach($this->getServer()->getLevels() as $level){
+                $list[] = $level->getName();
+            }
+            $level = $list[$data[0]];
+            $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+            $config->set("World", $level);
+            $config->save();
+            $player-> sendMessage("§eSet Level to: §b" . $level);
+            $this->openHvS($player);
+        });
+        $list = [];
+        foreach($this->getServer()->getLevels() as $level){
+            $list[] = $level->getName();
+        }
+		$form->setTitle("§l§bLevelUI");
+        $form->addDropdown("§ePick a Level!", $list);
+		$form->sendToPlayer($player);
+		return $form;
+	}
 }
